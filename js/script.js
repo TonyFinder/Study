976ba118055f1eb6f252e74696b87ace68c17bab
Anton <antonrozdobudko@gmail.com>
2020-12-19 16:00:52 +1000

    Изучение условий

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 "use.strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('not ok)');
+// }
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Очень много');
+// } else {
+//     console.log("Правильно!");
+// }
 
-console.log(personalMovieDB);
+// (num === 50) ? console.log("Правильно!") : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 30:
+        console.log('Неа');
+        break;
+    case 89:
+        console.log('Ноуп');
+        break;
+    case 50:
+        console.log('Ага');
+        break;
+    default:
+        console.log('В другой раз, неудачник');
+        break;
+}
