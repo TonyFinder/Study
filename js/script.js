cea319e0722de8d5f3433c247869b44c64f3e3d0
Anton <antonrozdobudko@gmail.com>
2021-01-03 19:00:00 +1000

    22. Передача по ссылке или по значению, Spread Оператор ES6-ES9

--- js/script.js
+++ js/script.js
@@ -1,37 +1,102 @@
 "use.strict";
 
-// const str = prompt("", "");
-// const products = str.split(", ");
-// products.sort();
-// console.log(products.join("; "));
+// let a = 5,
+//     b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 10,
+//     b: 5
+// };
+
+// const copy = obj; // Передаётся ссылка
+
+// copy.a = 20;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(obj) {
+//     let objNew = {};
+//     let key;
+//     for (key in obj) {
+//         objNew[key] = obj[key];
+//     }
+
+//     return objNew;
+// }
+
+const numbers = {
+    a: 5,
+    b: 10,
+    c: 25,
+    d: {
+        x: 35,
+        y: 2
+    }
+};
 
 
+// const newNumbers = copy(numbers);
 
-const arr = [10, 2, 38, 6, 18];
+// newNumbers.a = 18;
+// newNumbers.d.x = 58; //Здесь уже ссылка, потому что вложенная
 
-console.log(arr.sort(compareNum));
+// console.log(newNumbers);
+// console.log(numbers);
 
-function compareNum(a, b) {
-    return a - b;
+const add = {
+    e: 40,
+    f: 50
+};
+
+// console.log(Object.assign(numbers, add));
+
+// console.log(numbers);
+// console.log(add);
+
+const clone = Object.assign({}, add);
+clone.e = 12;
+console.log(add);
+console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'abl;lh;h';
+
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['youtube', 'rutube'],
+      blogs = ['livejournal', 'newcinemf'],
+      combo = [...video, ...blogs, 'big boss', 'famouse'];
+      
+console.log(combo);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-// arr.forEach (function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+const num = [4, 6, 23];
+log(...num);
 
-// arr[99] = 0;
-// console.log(arr.length);
-// console.log(arr);
+const array = ['a', 's', 'p'];
+const newAray = [...array];
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+console.log(newAray);
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+const obj = {
+    a: 5,
+    b: 12
+};
 
-// arr.pop();
-// arr.push(12);
+const newObj = {...obj};
 
-// console.log(arr);
+console.log(newObj);
